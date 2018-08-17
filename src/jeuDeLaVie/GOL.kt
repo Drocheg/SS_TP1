30bbd677d6c60ab3176979d90f2b0da8867696f3
Juan Franco <juan.caracciolo@wolox.com.ar>
2018-08-17 00:29:35 -0300

    Added commented values

--- src/jeuDeLaVie/GOL.kt
+++ src/jeuDeLaVie/GOL.kt
@@ -84,21 +84,31 @@ class GOL {
             @JvmStatic
             fun main(args: Array<String>) {
                 val epochs = 100
-                val rule = GenericRules(2,3,3,3)
-                val boardName = "glider3D_4555"
+//                val rule = GenericRules(2,3,3,3) //2DS
+//                val rule = GenericRules(2,3,2,4) //2DE
+//                val rule = GenericRules(3,5,4,4) //2DD
+//                val rule = GenericRules(9,9,3,3) //2DMG
+//                val rule = GenericRules(4,6,6,7) //3DS
+//                val rule = GenericRules(2,3,3,3) //3DE
+//                val rule = GenericRules(4,6,7,9) //3DD
+//                val rule = GenericRules(30,30,8,8) //3DMG
+//                val rule = GenericRules(4,5,5,5) //3DMG
+                val boardName = "oscillator"
                 val inputFileName = "golBoards/"+ boardName
 
-                val simulations = 10
+                val simulations = 20
                 val metricsList = mutableListOf<GOLMetrics>()
                 val rand = Random(120)
                 for (i in 0 until simulations) {
-                    val board = GOLRandomBoard.generate(40, 40, 40, 5, 5, 5, 0.2, seed = rand.nextLong(), boundZ = true)
-//                    val board = GOLBoardReader.generate(inputFileName)
-                    metricsList.add(run(board, epochs, rule, i == 0))
+//                    val board = GOLRandomBoard.generate(40, 40, 40, 5, 5, 5, 0.2, seed = rand.nextLong())
+//                    val board = GOLRandomBoard.generate(80, 80, 1, 8, 8, 1, 0.2, seed = rand.nextLong(), boundZ = true)
+//                    val board = GOLBoardReader.generate(inputFileName, boundZ = true)
+//                    metricsList.add(run(board, epochs, rule, i == 0))
+//                    metricsList.add(runAging(board, epochs, AgingRule(rule, 1), i == 0))
                 }
 
                 val aggregated = GOLAggregatedMetrics.getAggregatedMetrics(metricsList)
-                aggregated.output("output");
+                aggregated.output("output")
             }
     }
 }
