6a6577d52269f8e522ad07ebb8111838e6c37112
Juan Franco <juan.caracciolo@wolox.com.ar>
2018-08-16 21:20:46 -0300

    Graphic parameters

--- src/jeuDeLaVie/GOL.kt
+++ src/jeuDeLaVie/GOL.kt
@@ -14,7 +14,7 @@ class GOL {
                 epochs: Int,
                 rule: GOLRule,
                 save: Boolean = false,
-                maxDistance: Int = 25) : GOLMetrics {
+                maxDistance: Int = 20) : GOLMetrics {
             var oldBoard = initial
             var newboard = initial.clone()
 
@@ -84,23 +84,15 @@ class GOL {
             @JvmStatic
             fun main(args: Array<String>) {
                 val epochs = 100
-                val rule = GenericRules(4,6,6,7)
+                val rule = GenericRules(2,3,3,3)
                 val boardName = "glider3D_4555"
                 val inputFileName = "golBoards/"+ boardName
-                val outputFileName = boardName + "_"+ rule.name+"_" + epochs + "_"
-
-//                val inputFileName = "golBoards/exploder"
-//                val outputFileName = "dense"
-
-                val boundZ = true
-//            var oldBoard = GOLBoardReader.generate(inputFileName, boundZ = boundZ)
-//            var newboard = GOLBoardReader.generate(inputFileName, boundZ = boundZ)
 
                 val simulations = 10
                 val metricsList = mutableListOf<GOLMetrics>()
-                val rand = Random()
+                val rand = Random(120)
                 for (i in 0 until simulations) {
-                    val board = GOLRandomBoard.generate(100, 100, 100, 8, 8, 8, 0.2, seed = rand.nextLong(), boundZ = true)
+                    val board = GOLRandomBoard.generate(40, 40, 40, 5, 5, 5, 0.2, seed = rand.nextLong(), boundZ = true)
 //                    val board = GOLBoardReader.generate(inputFileName)
                     metricsList.add(run(board, epochs, rule, i == 0))
                 }
